ef6d07e234ca989a976111449d7bbc5237d0df91
BitLeigh Vu <bitleighvu@gatech.edu>
2020-01-15 14:20:55 -0500

    skeleton UI for home page

--- hema-membership-tracker/src/Components/Home/Home.js
+++ hema-membership-tracker/src/Components/Home/Home.js
@@ -5,14 +5,25 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
+    <div id="home-pg">
       <a id="text-header">Membership Tracker</a>
-      <input 
-        id="emailInput"
-        className="form-control form-control-lg"
-        type="email"
-        placeholder="johndoe@hema.com"
-      />
+      <div id="validation">
+        <input 
+          id="emailInput"
+          className="form-control form-control-lg"
+          type="email"
+          placeholder="johndoe@hema.com"
+        />
+        <button 
+          id="button-input"
+          className="btn btn-secondary btn-md"
+          >
+          VERIFY MEMBERSHIP
+        </button>
+      </div>
+      
+      <span id="message">Please input an email to check on their HEMAA membership status.</span>
+
     </div>
   );
 };
