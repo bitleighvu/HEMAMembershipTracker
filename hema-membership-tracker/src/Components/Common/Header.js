2b32e5fbed6a51d13b4f4e3f5590195ae112b3f6
BitLeigh Vu <bitleighvu@gatech.edu>
2020-01-20 16:27:10 -0500

    new header design

--- hema-membership-tracker/src/Components/Common/Header.js
+++ hema-membership-tracker/src/Components/Common/Header.js
@@ -4,12 +4,27 @@ import "./Header.css";
 const Header = () => {
   return (
     <div id="header"> 
-      <a target="_blank" href="https://www.hemaalliance.com/">
-      <img
-        src={process.env.PUBLIC_URL + "/images/logo.png"}
-        alt="Historical European Martial Arts Logo"
-        id="HEMALogo"/>
-      </a>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="https://www.hemaalliance.com/">
+            <img src={process.env.PUBLIC_URL + "/images/logo.png"} width="80" height="80" alt="Historical European Martial Arts Logo"/>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Membership Tracker<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Waiver Submission<span class="sr-only"></span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Event Sign-In<span class="sr-only"></span></a>
+              </li>
+            </ul>
+          </div>
+      </nav>
     </div>
   )
 };
