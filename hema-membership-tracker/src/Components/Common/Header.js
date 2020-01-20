139dd55c971a6c65384e0dde1a1a6e4e29adff93
BitLeigh Vu <bitleighvu@gatech.edu>
2020-01-20 17:12:59 -0500

    readability changes

--- hema-membership-tracker/src/Components/Common/Header.js
+++ hema-membership-tracker/src/Components/Common/Header.js
@@ -4,23 +4,23 @@ import "./Header.css";
 const Header = () => {
   return (
     <div id="header"> 
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="https://www.hemaalliance.com/">
-            <img src={process.env.PUBLIC_URL + "/images/logo.png"} width="80" height="80" alt="Historical European Martial Arts Logo"/>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="https://www.hemaalliance.com/">
+            <img src={process.env.PUBLIC_URL + "/images/logo.png"} id="logo" alt="Historical European Martial Arts Logo"/>
           </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Membership Tracker<span class="sr-only">(current)</span></a>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">Membership Tracker<span className="sr-only">(current)</span></a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Waiver Submission<span class="sr-only"></span></a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Waiver Submission<span className="sr-only"></span></a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Event Sign-In<span class="sr-only"></span></a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Event Sign-In<span className="sr-only"></span></a>
               </li>
             </ul>
           </div>
