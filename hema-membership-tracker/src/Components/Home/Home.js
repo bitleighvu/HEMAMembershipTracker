e856db4b4dc2b8658e4779d250a49dd26adfe7b7
Jarod Schneider <jschneider@gatech.edu>
2020-01-22 13:05:00 -0500

    spinner implemented

--- hema-membership-tracker/src/Components/Home/Home.js
+++ hema-membership-tracker/src/Components/Home/Home.js
@@ -1,31 +1,55 @@
 import React from "react";
 
 import "./Home.css";
+import { PulseLoader } from "react-spinners";
 
 
-const Home = () => {
-  return (
-    <div id="home-pg">
-      <a id="text-header">Membership Tracker</a>
-      <div id="validation">
-        <input 
-          id="emailInput"
-          className="form-control form-control-lg"
-          type="email"
-          placeholder="johndoe@hema.com"
-        />
-        <button 
-          id="button-input"
-          className="btn btn-secondary btn-md"
-          >
-          VERIFY MEMBERSHIP
-        </button>
-      </div>
-      
-      <span id="message">Please input an email to check on their HEMAA membership status.</span>
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            processing: false,
+        };
+    }
 
-    </div>
-  );
-};
+    render() {
+        return (
+            <div id="home-pg">
+                <a id="text-header">Membership Tracker</a>
+                <div id="validation">
+                    <div id={'input'}>
+                        <input
+                            id="emailInput"
+                            className="form-control form-control-lg"
+                            type="email"
+                            placeholder="johndoe@hema.com"
+                        />
+                    </div>
+                    <button
+                        id="button-input"
+                        className="btn btn-secondary btn-md"
+                        onClick={this._onClick.bind(this)}
+                    >
+                        {this.state.processing ? <PulseLoader color={"#fafafa"}/> : "VERIFY MEMBERSHIP"}
+                    </button>
+                </div>
+
+                <span id="message">Please input an email to check on their HEMAA membership status.</span>
+
+            </div>
+        );
+    }
+
+    _onClick() {
+        this.setState({
+            processing: true
+        });
+        setTimeout(() => {
+            this.setState({
+                processing: false
+            })
+        }, 2000);
+    }
+}
 
 export default Home;
