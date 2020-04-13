00fbb845474a1c5d88410f5f43c9e993ee528a7f
Sean Walsh <seanwalsh@gatech.edu>
2020-04-13 04:56:02 -0400

    Local changes from sprint 4

--- hema-membership-tracker/src/Helpers/Parser.js
+++ hema-membership-tracker/src/Helpers/Parser.js
@@ -1,15 +1,21 @@
 const fs = require('fs')
 const papa = require('papaparse')
-const file = fs.createReadStream('memberships_2020-02-05_16_55_18.csv')
+// const file = fs.createReadStream('memberships_2020-02-05_16_55_18.csv')
 
-var count = 0
+function parseData(data, target) {
+    papa.parse(data, {
+        header: true,
+        worker: true,
+        delimiter: "",
+        complete: function(result) {
+            for (var index = 0; index < result.length; index++) {
+                if (result.email == target) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    });
+}
 
-papa.parse(file, {
-    worker: true,
-    step: function(result) {
-
-    },
-    complete: function(results, file) {
-        console.log('Parsing complete, there are ', count, ' entries.')
-    }
-})
+export default { parseData };
